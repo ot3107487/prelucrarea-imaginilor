ef21109d306b2a897147a81d500b5c770a6b72d2
ot3107487 <Gygabyte12>
2019-04-07 16:39:16 +0300

    Lab 03- filtrare directionala

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -16,12 +16,11 @@ import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
 import static java.lang.Math.pow;
 
 public class Controller {
@@ -76,9 +75,9 @@ public class Controller {
         }
         int width = img.getWidth();
         int height = img.getHeight();
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                this.pixelTransform(img, i, j, this.selectedTransformationType);
+        for (int i = 1; i < width - 1; i++) {
+            for (int j = 1; j < height - 1; j++) {
+                this.areaTransform(img, i, j, this.selectedTransformationType);
             }
         }
 
@@ -92,31 +91,192 @@ public class Controller {
         this.output.setImage(new Image("file:out.png"));
     }
 
-    private void pixelTransform(BufferedImage img, int posX, int posY, TransformationType transformationType) {
-        switch (transformationType) {
-            case NOISE_REDUCTION: {
-                this.noiseReduction(img, posX, posY);
+    /***
+     *
+     * @param img
+     * @param i position x in image of the centered element in the mask
+     * @param j position x in image of the centered element in the mask
+     * @param selectedTransformationType
+     */
+    private void areaTransform(BufferedImage img, int i, int j, TransformationType selectedTransformationType) {
+        switch (selectedTransformationType) {
+            case FILTRARE_DIRECTIONALA: {
+                this.filteringDirection(img, i, j);
                 break;
             }
-            case BIT_EXTRACTION_0: {
-                this.bitExtraction(img, posX, posY, 0);
+            case IMAGINI_MEDICAE: {
+                this.medicalImages(img, i, j);
                 break;
             }
-            case BIT_EXTRACTION_1: {
-                this.bitExtraction(img, posX, posY, 1);
-                break;
-            }
-            case GAMMA_CORRECTION: {
-                this.gammaCorrection(img, posX, posY, 1);
+            case INVERSAREA_CONSTRASTULUI: {
+                this.constrastInversion(img, i, j);
                 break;
             }
             default:
                 break;
         }
+
     }
 
-    private void gammaCorrection(BufferedImage img, int posX, int posY, int constant) {
-        int p = img.getRGB(posX, posY);
+    private void constrastInversion(BufferedImage img, int i, int j) {
+        int[][] mask = new int[3][3];
+        mask[1][1] = this.pixelGrayScale(img, i, j);
+
+        int x = mask[0][0];
+    }
+
+    private void medicalImages(BufferedImage img, int i, int j) {
+    }
+
+    private void filteringDirection(BufferedImage img, int i, int j) {
+        int r = this.maskRed(img, i, j);
+        int g = this.maskGreen(img, i, j);
+        int b = this.maskBlue(img, i, j);
+        int val = (255 << 24) | (r << 16) | (g << 8) | b;
+        img.setRGB(i, j, val);
+
+    }
+
+    private int maskBlue(BufferedImage img, int i, int j) {
+        int[][] mask = new int[3][3];
+        for (int m = 0; m < 3; m++) {
+            for (int n = 0; n < 3; n++) {
+                mask[m][n] = pixelBlue(img, i + m - 1, j + n - 1);
+            }
+        }
+        int medieOrizontala = (mask[1][0] + mask[1][1] + mask[1][2]) / 3;
+        int medieVerticala = (mask[1][1] + mask[0][1] + mask[0][2]) / 3;
+        int medieDiagonalaPrincipala = (mask[0][0] + mask[1][1] + mask[2][2]) / 3;
+        int medieDiagonalaSecundara = (mask[2][0] + mask[1][1] + mask[0][2]) / 3;
+        int minim = -1;
+        int diferentaMinima = 256;
+        int valoareMijloc = mask[1][1];
+        if (abs(valoareMijloc - medieDiagonalaPrincipala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaPrincipala);
+            minim = medieDiagonalaPrincipala;
+        }
+        if (abs(valoareMijloc - medieDiagonalaSecundara) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaSecundara);
+            minim = medieDiagonalaSecundara;
+        }
+        if (abs(valoareMijloc - medieVerticala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieVerticala);
+            minim = medieVerticala;
+        }
+        if (abs(valoareMijloc - medieOrizontala) < diferentaMinima) {
+            minim = medieOrizontala;
+        }
+        return minim;
+
+    }
+
+    private int maskRed(BufferedImage img, int i, int j) {
+        int[][] mask = new int[3][3];
+        for (int m = 0; m < 3; m++) {
+            for (int n = 0; n < 3; n++) {
+                mask[m][n] = pixelRed(img, i + m - 1, j + n - 1);
+            }
+        }
+        int medieOrizontala = (mask[1][0] + mask[1][1] + mask[1][2]) / 3;
+        int medieVerticala = (mask[1][1] + mask[0][1] + mask[0][2]) / 3;
+        int medieDiagonalaPrincipala = (mask[0][0] + mask[1][1] + mask[2][2]) / 3;
+        int medieDiagonalaSecundara = (mask[2][0] + mask[1][1] + mask[0][2]) / 3;
+        int minim = -1;
+        int diferentaMinima = 256;
+        int valoareMijloc = mask[1][1];
+        if (abs(valoareMijloc - medieDiagonalaPrincipala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaPrincipala);
+            minim = medieDiagonalaPrincipala;
+        }
+        if (abs(valoareMijloc - medieDiagonalaSecundara) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaSecundara);
+            minim = medieDiagonalaSecundara;
+        }
+        if (abs(valoareMijloc - medieVerticala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieVerticala);
+            minim = medieVerticala;
+        }
+        if (abs(valoareMijloc - medieOrizontala) < diferentaMinima) {
+            minim = medieOrizontala;
+        }
+        return minim;
+    }
+
+    private int maskGreen(BufferedImage img, int i, int j) {
+        int[][] mask = new int[3][3];
+        for (int m = 0; m < 3; m++) {
+            for (int n = 0; n < 3; n++) {
+                mask[m][n] = pixelGreen(img, i + m - 1, j + n - 1);
+            }
+        }
+        int medieOrizontala = (mask[1][0] + mask[1][1] + mask[1][2]) / 3;
+        int medieVerticala = (mask[1][1] + mask[0][1] + mask[0][2]) / 3;
+        int medieDiagonalaPrincipala = (mask[0][0] + mask[1][1] + mask[2][2]) / 3;
+        int medieDiagonalaSecundara = (mask[2][0] + mask[1][1] + mask[0][2]) / 3;
+        int minim = -1;
+        int diferentaMinima = 256;
+        int valoareMijloc = mask[1][1];
+        if (abs(valoareMijloc - medieDiagonalaPrincipala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaPrincipala);
+            minim = medieDiagonalaPrincipala;
+        }
+        if (abs(valoareMijloc - medieDiagonalaSecundara) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieDiagonalaSecundara);
+            minim = medieDiagonalaSecundara;
+        }
+        if (abs(valoareMijloc - medieVerticala) < diferentaMinima) {
+            diferentaMinima = abs(valoareMijloc - medieVerticala);
+            minim = medieVerticala;
+        }
+        if (abs(valoareMijloc - medieOrizontala) < diferentaMinima) {
+            minim = medieOrizontala;
+        }
+        return minim;
+    }
+
+    private int pixelBlue(BufferedImage img, int i, int j) {
+        int p = img.getRGB(i, j);
+        return p & 0xff;
+    }
+
+    private int pixelGreen(BufferedImage img, int i, int j) {
+        int p = img.getRGB(i, j);
+        return (p >> 8) & 0xff;
+    }
+
+    private int pixelRed(BufferedImage img, int i, int j) {
+        int p = img.getRGB(i, j);
+        return (p >> 16) & 0xff;
+    }
+
+    private void grayScale() {
+        BufferedImage img = null;
+        File f = null;
+        try {
+
+            f = new File("in.png");
+            img = ImageIO.read(f);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        int width = img.getWidth();
+        int height = img.getHeight();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.pixelGrayScale(img, i, j);
+            }
+        }
+
+        //write image
+        try {
+            ImageIO.write(img, "png", f);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private int pixelGrayScale(BufferedImage img, int i, int j) {
+        int p = img.getRGB(i, j);
         //get red
         int r = (p >> 16) & 0xff;
 
@@ -129,83 +289,13 @@ public class Controller {
         //gray scale
         int u = (r + g + b) / 3;
         int a = 255;
-        int L = 255;
-        double gamma = this.gammaSlider.getValue();
-        double baza = (double) u / 255;
-        u = (int) (constant * 255 * pow((double) u / 255, gamma * constant));
         r = u;
         g = u;
         b = u;
         //set the pixel value
         p = (a << 24) | (r << 16) | (g << 8) | b;
-        img.setRGB(posX, posY, p);
-    }
-
-    private void bitExtraction(BufferedImage img, int posX, int posY, int order) {
-        // image should be gray scale
-        int p = img.getRGB(posX, posY);
-        //get red
-        int r = (p >> 16) & 0xff;
-
-        //get green
-        int g = (p >> 8) & 0xff;
-
-        //get blue
-        int b = p & 0xff;
-
-        //gray scale
-        int u = (r + g + b) / 3;
-        int a = 255;
-        int L = 255;
-        while (order != 0) {
-            u = u >> 1;
-            order--;
-        }
-        u = L * (u % 2);
-        r = u;
-        g = u;
-        b = u;
-        //set the pixel value
-        p = (a << 24) | (r << 16) | (g << 8) | b;
-        img.setRGB(posX, posY, p);
-    }
-
-    private void noiseReduction(BufferedImage img, int posX, int posY) {
-        // image should be gray scale
-        int p = img.getRGB(posX, posY);
-        //get red
-        int r = (p >> 16) & 0xff;
-
-        //get green
-        int g = (p >> 8) & 0xff;
-
-        //get blue
-        int b = p & 0xff;
-
-        //gray scale
-        int u = (r + g + b) / 3;
-
-        int a = 255;
-
-
-        int va = (int) this.aSlider.getValue();
-        int vb = (int) this.bSlider.getValue();
-        int L = 255;
-        if (u <= va) {
-            u = 0;
-        }
-        if (u > va && u <= vb) {
-            u = (u - va) * L / (vb - va);
-        }
-        if (u > vb) {
-            u = L;
-        }
-        r = u;
-        g = u;
-        b = u;
-        //set the pixel value
-        p = (a << 24) | (r << 16) | (g << 8) | b;
-        img.setRGB(posX, posY, p);
+        img.setRGB(i, j, p);
+        return u;
     }
 
 
@@ -221,6 +311,7 @@ public class Controller {
         }
         File newInput = new File("./in.png");
         ImageIO.write(img, "png", newInput);
+        // this.grayScale();
         this.input.setImage(new Image("file:in.png"));
     }
 
@@ -282,28 +373,5 @@ public class Controller {
                 System.out.println(e);
             }
         }
-    }
-
-    private void SaveFile(String content, File file) {
-        try {
-            FileWriter fileWriter = null;
-
-            fileWriter = new FileWriter(file);
-            fileWriter.write(content);
-            fileWriter.close();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-    }
-
-    private static String readAllBytesJava7(String filePath) {
-        String content = "";
-        try {
-            content = new String(Files.readAllBytes(Paths.get(filePath)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return content;
     }
 }
